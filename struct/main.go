89c6cfe41ee0397e1de33b9c05483449a2e6e028
bruteforce1414 <bruteforce1414@gmail.com>
2018-11-26 12:41:59 +0500

    Задача выполнена

--- struct/main.go
+++ struct/main.go
@@ -3,11 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	p := Person{}
 
+	p := Person{}
 	p.SetAge(21)
 	p.SetName("John Doe")
 	p.SetEmail("john.doe@example.com")
-
 	fmt.Printf("%s %d : %s", p.GetName(), p.GetAge(), p.GetEmail())
 }
--- struct/struct.go
+++ struct/struct.go
@@ -10,25 +10,26 @@ type Person struct {
 }
 
 func (P *Person) GetName() string {
-	//
+	return P.name
 }
 
 func (P *Person) GetEmail() string {
-	//
+	return P.email
 }
 
 func (P *Person) GetAge() int {
-	//
+	return P.age
 }
 
 func (P *Person) SetName(name string) {
-	//
+	P.name=name;
 }
 
 func (P *Person) SetEmail(email string) {
-	//
+	P.email=email;
 }
 
 func (P *Person) SetAge(age int) {
-	//
+	P.age=age;
+
 }
