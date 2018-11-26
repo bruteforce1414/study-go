82092551abd4e6d4486be5801f54945039871487
bruteforce1414 <bruteforce1414@gmail.com>
2018-11-26 16:25:13 +0500

    Изменена структура тестовых каталогов и пути к ним в коде программы

--- interfaces/main.go
+++ interfaces/main.go
@@ -10,8 +10,8 @@ import (
 // флаг --dir указывает путь к директории из которой нужно загрузить все файлы как плагины
 func main() {
 	m := GetPluginManager()
-    dir:=flag.String("path","D:\\development\\GoProjects\\study-go\\interfaces\\testplugins","a string")
-	plugin:=flag.String("pathFile","D:\\development\\GoProjects\\study-go\\interfaces\\testpluginsSingle\\singleplugin.txt","a string")
+    dir:=flag.String("path","D:\\development\\GoProjects\\study-go\\interfaces\\testdata\\plugins","a string")
+	plugin:=flag.String("pathFile","D:\\development\\GoProjects\\study-go\\interfaces\\testdata\\pluginSingle\\singleplugin.txt","a string")
 
 
 	// тут нужно получить данные из флагов, если флаги указаны
