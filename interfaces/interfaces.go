409d64e83b31161a9d34dec93f698208ead0a12e
bruteforce1414 <bruteforce1414@gmail.com>
2018-11-26 16:15:54 +0500

    Задача выполнена

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -1,6 +1,10 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"io/ioutil"
+	"sync"
+)
 
 var instance *manager
 var once sync.Once
@@ -29,13 +33,33 @@ func GetPluginManager() PluginManager {
 }
 
 func (m *manager) AddDir(path string) {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Println(err)
+		}
+	for _, file := range files {
+		m.plugins=append(m.plugins,path+file.Name())
 
+	}
 }
 
-func (m *manager) AddFile(path string) {
 
+func (m *manager) AddFile(path string) {
+	_, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Println(err)
+		}else{
+		m.plugins=append(m.plugins,path)
+	}
 }
 
 func (m *manager) List() []string {
-
+	if len(m.plugins)==0 {
+		fmt.Println("\nПлагинов нет!!!")
+	}else {
+		fmt.Println("\nСписок загруженных плагинов:\n")
+		for _, value := range m.plugins {
+			fmt.Println(value)}
+		}
+	return m.plugins
 }
--- interfaces/main.go
+++ interfaces/main.go
@@ -1,20 +1,35 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 // Программа может принять два флага @see https://golang.org/pkg/flag/
 // флаг --plugin указывает путь к файлу, который нужно загрузить
 // флаг --dir указывает путь к директории из которой нужно загрузить все файлы как плагины
 func main() {
 	m := GetPluginManager()
+    dir:=flag.String("path","D:\\development\\GoProjects\\study-go\\interfaces\\testplugins","a string")
+	plugin:=flag.String("pathFile","D:\\development\\GoProjects\\study-go\\interfaces\\testpluginsSingle\\singleplugin.txt","a string")
+
 
 	// тут нужно получить данные из флагов, если флаги указаны
 
-	if {
-		m.AddDir()
+	if dir!=nil{
+		m.AddDir(*dir)
+	}else {
+		fmt.Println("Флаг --dir не установлен")
 	}
 
-	if {
-		m.AddFile()
+
+	if plugin!=nil{
+		m.AddFile(*plugin)
+	}else {
+		fmt.Println("Флаг --plugin не установлен")
 	}
+
+     m.List()
 
 	// выводим список всех плагинов, если список пуст, то выводим "плагинов нет"
 }
